87865813704548261da28daa2c12e9155e91db51
Akina Amegan <akinaamegan@MacBook-Pro-dAkina--2.local>
2026-05-19 23:57:45 +0200

    fix backend cors for vercel frontend

--- backend/server.py
+++ backend/server.py
@@ -342,8 +342,12 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        "https://beautifyvision.vercel.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
